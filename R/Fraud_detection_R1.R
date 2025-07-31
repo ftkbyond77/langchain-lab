7871ff7d7b732188b4d62488bb889c2ae4388375
Chayut Meekhun <chayutmeekhun4@gmail.com>
2025-07-31 14:06:06 +0700

    R set up

---
+++ R/Fraud_detection_R1.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/BM MONEY/OneDrive/Desktop/mycode/langchain-lab/langchain-lab/Ai Analyst/data")
+data <- read.csv("PS_20174392719_1491204439457_log.csv")
+print(data)
+head(data)
+
+str(data)
